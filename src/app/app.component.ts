8b6407aa2faa7f7fc8f8fe4883c4237a4cec194e
tammaro <tammaroimpicciatore@hotmail.com>
2020-02-21 21:51:49 +0100

    gg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { DeviceDetectorService } from 'ngx-device-detector';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  deviceInfo = null;
+  isMobile: boolean;
+  isDesktop: boolean;
+  constructor(private deviceService: DeviceDetectorService) {
+    this.deviceInfo = this.deviceService.getDeviceInfo();
+    this.isMobile = this.deviceService.isMobile();
+    this.isDesktop = this.deviceService.isDesktop();
+  }
   title = 'frontend-otaku-world';
 }
